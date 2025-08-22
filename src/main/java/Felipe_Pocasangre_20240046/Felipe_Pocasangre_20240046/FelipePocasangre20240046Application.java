5ae17ee01011724a9d82d27259fcb3a4dc4fa3a5
Gatom62 <20240046@ricaldone.edu.sv>
2025-08-22 10:30:09 -0600

    Pruba POO

---
+++ src/main/java/Felipe_Pocasangre_20240046/Felipe_Pocasangre_20240046/FelipePocasangre20240046Application.java
@@ -0,0 +1,13 @@
+package Felipe_Pocasangre_20240046.Felipe_Pocasangre_20240046;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelipePocasangre20240046Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelipePocasangre20240046Application.class, args);
+	}
+
+}
---
+++ src/test/java/Felipe_Pocasangre_20240046/Felipe_Pocasangre_20240046/FelipePocasangre20240046ApplicationTests.java
@@ -0,0 +1,13 @@
+package Felipe_Pocasangre_20240046.Felipe_Pocasangre_20240046;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelipePocasangre20240046ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
